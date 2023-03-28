44712750dc1402b358f1be30d2d3c98cf478bfb0
borela <borela@tcu.gov.br>
2023-03-28 14:52:48 -0300

    execucao local com gpu maior

--- code/teste.py
+++ code/teste.py
@@ -1,3 +1,3 @@
-from pyserini.search import SimpleSearcher
-searcher = SimpleSearcher.from_prebuilt_index("trec-dl 2020")
+# from pyserini.search import SimpleSearcher
+# searcher = SimpleSearcher.from_prebuilt_index("trec-dl 2020")
 print('oi')
